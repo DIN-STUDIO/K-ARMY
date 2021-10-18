acd2168bbbe2908859586298c1c16209baf30cc4
HyoTaek <86708287+DIN-STUDIO@users.noreply.github.com>
2021-10-18 23:40:24 +0900

    Add files via upload

--- JS/login.js
+++ JS/login.js
@@ -1,8 +1,10 @@
 const loginForm = document.querySelector("#loginform")
+const loginMain = document.querySelector("#main")
 const img1 = document.querySelector("img1")
 
 function formHide(){
     loginForm.classList.add("hidden")
+    loginMain.classList.remove("hidden")
 }
 
 function onSubmit(event){
